14a1dd900326e0270132a14fd9fa95769aba8f13
Kirill Zolotaryov <252222223a@gmail.com>
2023-06-16 03:53:42 +0300

    vk commit

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -55,7 +55,7 @@ export const authOptions: NextAuthOptions = {
   // },
   // adapter: PrismaAdapter(prisma),
   providers: [
-    VkProvider({
+    VK({
       clientId: env.VK_CLIENT_ID,
       clientSecret: env.VK_CLIENT_SECRET
     }),
@@ -101,3 +101,63 @@ export const getServerAuthSession = (ctx: {
 }) => {
   return getServerSession(ctx.req, ctx.res, authOptions);
 };
+
+
+import {OAuthConfig, OAuthUserConfig} from 'next-auth/providers';
+
+interface VkProfile {
+    response: Array<{
+        id: number;
+        first_name: string;
+        last_name: string;
+        photo_100: string;
+        can_access_closed: boolean;
+        is_closed: boolean;
+    }>;
+}
+
+export default function VK<P extends Record<string, any> = VkProfile>(
+    options: OAuthUserConfig<P>,
+): OAuthConfig<P> {
+    const apiVersion = '5.131'; // https://vk.com/dev/versions
+
+    return {
+        id: 'vk',
+        name: 'VK',
+        type: 'oauth',
+        authorization: `https://oauth.vk.com/authorize?scope=email&v=${apiVersion}`,
+        token: {
+            url: `https://oauth.vk.com/access_token?v=${apiVersion}`,
+            async request({client, params, checks, provider}) {
+                // exclude user_id and email from response
+                const {user_id, email, ...tokens} = await client.oauthCallback(
+                    provider.callbackUrl,
+                    params,
+                    checks,
+                    {
+                        exchangeBody: {
+                            client_id: options.clientId,
+                            client_secret: options.clientSecret,
+                        },
+                    },
+                );
+
+                return {tokens};
+            },
+        },
+        userinfo: `https://api.vk.com/method/users.get?fields=photo_100&v=${apiVersion}`,
+        profile(result: P) {
+            const profile = result.response?.[0] ?? {};
+
+            return {
+                id: profile.id,
+                name: [profile.first_name, profile.last_name]
+                    .filter(Boolean)
+                    .join(' '),
+                email: null,
+                image: profile.photo_100,
+            };
+        },
+        options,
+    };
+}
